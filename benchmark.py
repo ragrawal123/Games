3d3015567f98d6f235b59af1c2ad75fa8e1b5755
marvin <marvinjmok@yahoo.com>
2020-12-13 21:06:31 -0800

    final stuff

--- benchmark.py
+++ benchmark.py
@@ -11,6 +11,7 @@ import percolator
 import percolator2
 from util import *
 w= 0 
+g = 0
 
 # Removes the given vertex v from the graph, as well as the edges attached to it.
 # Removes all isolated vertices from the graph as well.
@@ -28,7 +29,7 @@ class TimeoutError(Exception):
     pass
 
 class Timeout:
-    def __init__(self, seconds=2, error_message="Timeout of {0} seconds hit"):
+    def __init__(self, seconds=1, error_message="Timeout of {0} seconds hit"):
         self.seconds = seconds
         self.error_message = error_message.format(seconds)
     def handle_timeout(self, signum, frame):
@@ -81,7 +82,7 @@ def PlayGraph(s, t, graph):
     # Check that all vertices are colored now.
     assert all(v.color != -1 for v in graph.V)
 
-    #w += percolator.PercolationPlayer.auxwinnable(percolator.GoodGraph(graph.V,graph.E), 0)[0]
+    #w += percolator.wrapperM(percolator.GoodGraph(graph.V,graph.E), 0)[0]
     # Phase 2: Removal phase
     # Continue while both players have vertices left to remove.
     while len([v for v in graph.V if v.color == active_player]) > 0:
@@ -128,12 +129,16 @@ def BinomialRandomGraph(k, p):
 
 # This method creates and plays a number of random graphs using both passed in players.
 def PlayBenchmark(p1, p2, iters):
+    global g
     graphs = (
         BinomialRandomGraph(random.randint(2,20), random.random())
         for _ in range(iters)
     )
     wins = [0, 0]
     for graph in graphs:
+        
+        if len(graph.E) == 1 or len(graph.E) == 0:
+            g+=1
         g1 = copy.deepcopy(graph)
         g2 = copy.deepcopy(graph)
         # Each player gets a chance to go first on each graph.
@@ -145,6 +150,42 @@ def PlayBenchmark(p1, p2, iters):
         wins[1-winner_b] += 1
     return wins
 
+def part1(graph, s, t):
+    global w
+    players = [s, t]
+    active_player = 0
+
+    # Phase 1: Coloring Phase
+    while any(v.color == -1 for v in graph.V):
+        # First, try to just *run* the player's code to get their vertex.
+        try:
+            with Timeout():
+                chosen_vertex = players[active_player].ChooseVertexToColor(copy.deepcopy(graph), active_player)
+
+        # If user code does not return within appropriate timeout, select random action.
+        except TimeoutError as e:
+            print(e)
+            traceback.print_exc(file=sys.stdout)
+            chosen_vertex = RandomPlayer.ChooseVertexToColor(copy.deepcopy(graph), active_player)
+        except Exception as e:
+            traceback.print_exc(file=sys.stdout)
+            return 1 - active_player
+        # Next, check that their output was reasonable.
+        try:
+            original_vertex = GetVertex(graph, chosen_vertex.index)
+            if not original_vertex:
+                return 1 - active_player
+            if original_vertex.color != -1:
+                return 1 - active_player
+            # If output is reasonable, color this vertex.
+            original_vertex.color = active_player
+        # Only case when this should fire is if chosen_vertex.index does not exist or similar error.
+        except Exception as e:
+            traceback.print_exc(file=sys.stdout)
+            return 1 - active_player
+
+        # Swap current player.
+        active_player = 1 - active_player
 
 # This is a player that plays a legal move at random.
 class RandomPlayer:
@@ -167,7 +208,8 @@ if __name__ == "__main__":
     # from percolator import PercolationPlayer
     # p1 = PercolationPlayer
     p2 = RandomPlayer
-    iters = 100
+    iters = 200
+    wins = [1,1]
     wins = PlayBenchmark(p1, p2, iters)
     print(wins)
     print(
@@ -175,4 +217,32 @@ if __name__ == "__main__":
             1.0 * wins[0] / sum(wins), 1.0 * wins[1] / sum(wins)
         )
     )
+    tw = (wins[0] - g) / (wins[1] + wins[0] - (2 * g))
+    print('true wr: ' + str(tw))
     print(w)
+    print(g)
+
+    l1 = []
+    l2 = []
+    for i in range(10):
+        break
+        g = BinomialRandomGraph(9, 0.5)
+        part1(g, RandomPlayer, RandomPlayer)
+        #print(g)
+        g = percolator.GoodGraph(g.V, g.E)
+        g.Percolate(GetVertex(g, 1))
+        #print(g)
+        percolator.PercolationPlayer.vremove(g, 0)
+        l1.append((percolator.PercolationPlayer.vremove(g, 0)[0], percolator.PercolationPlayer.vremove(g, 0)[1].index))
+        l2.append(percolator.vremoveWrapper2(g, 0))
+    #print(l1)
+    #print(l2)
+    #print(l1 == l2)
+
+
+   
+
+
+
+
+
